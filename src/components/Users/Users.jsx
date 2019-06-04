19311f68355be482db3cdab30939ebeb82177a9d
NikitaM108 <50235344+NikitaM108@users.noreply.github.com>
2019-06-04 16:28:43 +0300

    add lifecycle methods

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,19 +5,13 @@ import userPhoto from '../../assets/images/images.png';
 
 
 class Users extends React.Component{
-	constructor(props) {
-		super(props);
-		
-			axios.get("https://social-network.samuraijs.com/api/1.0/users")
+	componentDidMount(){
+		axios.get("https://social-network.samuraijs.com/api/1.0/users")
 			.then(response=>{
 			this.props.setUsers(response.data.items)
 			});
-		
-
 	}
 	 
-	
-	
 	render(){
 	return <div>
 		{
