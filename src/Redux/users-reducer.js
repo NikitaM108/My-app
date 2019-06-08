edcd450256b479f141dd2065b9f777c0ada11222
NikitaM108 <50235344+NikitaM108@users.noreply.github.com>
2019-06-08 14:38:44 +0300

    add preloader

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT ='SET_TOTAL_USERS_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 let initialState = {
 	users: [],
 	pageSize: 5,
 	totalUsersCount: 0,
-	currentPage: 2
+	currentPage: 2,
+	isFetching: false
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -54,6 +56,11 @@ const usersReducer = (state = initialState, action) => {
 					
 					 ...state, totalUsersCount: action.count
 				}
+				case TOGGLE_IS_FETCHING:
+					return {
+						
+						 ...state, isFetching: action.isFetching
+					}
 
 		default:
 			return state;
@@ -61,10 +68,11 @@ const usersReducer = (state = initialState, action) => {
 	}
 }
 
-export const FollowAC = (userId) => ({ type: FOLLOW, userId })
-export const UnfollowAC = (userId) => ({ type: UNFOLLOW, userId })
-export const SetUsersAC = (users) => ({ type: SET_USERS, users })
-export const SetCurrentPageAC =(currentPage) =>({type:SET_CURRENT_PAGE, currentPage})
-export const SetTotalUsersCountAC =(totalUsersCount) =>({type:SET_TOTAL_USERS_COUNT, count: totalUsersCount})
+export const follow = (userId) => ({ type: FOLLOW, userId })
+export const unfollow = (userId) => ({ type: UNFOLLOW, userId })
+export const setUsers = (users) => ({ type: SET_USERS, users })
+export const setCurrentPage =(currentPage) =>({type:SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCount =(totalUsersCount) =>({type:SET_TOTAL_USERS_COUNT, count: totalUsersCount})
+export const toggleIsFetching =(isFetching) =>({type:TOGGLE_IS_FETCHING, isFetching })
 
 export default usersReducer;
