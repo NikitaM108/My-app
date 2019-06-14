75e8791cbcd49ab103e083df5c4afbdb3ad9466c
NikitaM108 <50235344+NikitaM108@users.noreply.github.com>
2019-06-14 14:17:41 +0300

    thunk 2

--- src/Redux/auth-reducer.js
+++ src/Redux/auth-reducer.js
@@ -1,3 +1,5 @@
+import { authAPI } from "../api/api";
+
 const SET_USER_DATA = 'SET_USER_DATA';
 
 
@@ -12,12 +14,12 @@ let initialState = {
 const authReducer = (state = initialState, action) => {
 
 	switch (action.type) {
-		case  SET_USER_DATA:
+		case SET_USER_DATA:
 			return {
 				...state,
 				...action.data,
-				isAuth:true
-				
+				isAuth: true
+
 			}
 
 
@@ -27,7 +29,17 @@ const authReducer = (state = initialState, action) => {
 	}
 }
 
-export const setAuthUserData = (id, email, login ) => ({ type: SET_USER_DATA, data: {id, email, login}})
+export const setAuthUserData = (id, email, login) => ({ type: SET_USER_DATA, data: { id, email, login } })
 
+export const getAuthUserData = () => (dispatch) => {
+	authAPI.me()
+			.then(response => {
+				if (response.data.resultCode === 0) {
+					let{id, email, login} = response.data.data;
+					dispatch(setAuthUserData(id, email, login));
+				}
+			});
+
+}
 
 export default authReducer;
--- src/api/api.js
+++ src/api/api.js
@@ -3,28 +3,42 @@ import * as axios from 'axios';
 const instance = axios.create({
 
 	withCredentials: true,
-	baseURL: 'https://social-network.samuraijs.com/api/1.0/', 
+	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
 	headers: {
 		"API-KEY": "8b6d6d36-525c-4671-8e20-3454fab7f0a1"
 	}
 });
 
 export const usersAPI = {
-getUsers(currentPage, pageSize)  {
-	return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-		.then(response => {
-			return response.data;
-		});
-},
-follow(userId){
-	return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
-						
+	getUsers(currentPage, pageSize) {
+		return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+			.then(response => {
+				return response.data;
+			});
+	},
+	follow(userId) {
+		return instance.post(`follow/${userId}`)
 
-		
-},
-unfollow(userId){
-	return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+
+
+	},
+	unfollow(userId) {
+		return instance.delete(`follow/${userId}`)
+	},
+
+	getProfile(userId) {
+		return instance.get(`profile/` + userId);
+
+	},
+
+
 }
+
+export const authAPI = {
+
+	me() { return instance.get(`auth/me`) 
+
+},
 
 }
 
